6e53880d5f681e74318045a1432dff75dd707aeb
Peter Heylin <peter@peterheylin.com>
2019-02-26 12:19:31 +0000

    20190226 testing bluetooth connection

---
+++ script.js
@@ -0,0 +1,31 @@
+function connect(){
+  // Step 1: Scan for a device with 0xffe5 service
+  navigator.bluetooth.requestDevice({
+    filters: [{ services: [0x6e7d] }]
+  })
+    .then(function(device) {
+      // Step 2: Connect to it
+      return device.gatt.connect();
+    })
+    .then(function(server) {
+      // Step 3: Get the Service
+      return server.getPrimaryService(0x6e7d);
+    })
+    .then(function(service) {
+      // Step 4: get the Characteristic
+      return service.getCharacteristic(0x6e7d);
+    })
+    // .then(function(characteristic) {
+    //   // Step 5: Write to the characteristic
+    //   var data = new Uint8Array([0xbb, 0x25, 0x05, 0x44]);
+    //   return characteristic.writeValue(data);
+    // })
+    .catch(function(error) {
+       // And of course: error handling!
+       console.error('Connection failed!', error);
+    });
+}
+
+// function listen() {
+//     annyang.start({ continuous: true });
+// }
